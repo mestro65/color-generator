45c562272554562ef10ee7b451c6ecf39fde53f2
mestro65 <67430555+mestro65@users.noreply.github.com>
2021-08-13 16:47:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ function setColor() {
     body.style.backgroundColor = input.value;
     colorCode.innerHTML = input.value;
 }
+
+
+
+function copied() {
+
+    var btn = document.getElementById("btn-copy");
+
+    if (btn.value == "Copy") {
+        btn.value = "Copied!";
+        btn.innerHTML = "Copied!";
+    }
+    else {
+        btn.value = "Copy";
+        btn.innerHTML = "Copy";
+    }
+
+}
